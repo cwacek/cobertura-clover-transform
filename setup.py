fcda6528f7ee85e2fb34bb21cc9e71b60eca4cf7
Chris <cwacek@gmail.com>
2014-07-22 11:07:08 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cobertura-clover-transform",
-    version='1.1',
+    version='1.1.1',
     packages=find_packages(),
     include_package_data=True,
     description="Tools for transforming Cobertura test "
