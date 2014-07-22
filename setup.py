d2cec288265165528e57e36563882d99425e8b6e
Chris <cwacek@gmail.com>
2014-07-22 09:32:09 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="cobertura-clover-transform",
-    version='1.0rc1',
+    version='1.0',
     packages=find_packages(),
     description="Tools for transforming Cobertura test "
                 "coverage XML into Clover-style XML",
     author='Chris Wacek',
+    author_email='cwacek@gmail.com',
     url='http://github.com/cwacek/cobertura-clover-transform',
     license='MIT',
     keywords='cobertura coverage test clover xml'
