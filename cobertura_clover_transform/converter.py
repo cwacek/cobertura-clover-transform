cfc3cb915a31615d5384cfe181582d915a097db6
Chris <cwacek@gmail.com>
2014-07-22 09:14:28 -0400

    Updated to use pkg_resources properly

--- cobertura_clover_transform/converter.py
+++ cobertura_clover_transform/converter.py
@@ -1,13 +1,13 @@
 import lxml.etree as ET
 import argparse
-import pkgutil
+import pkg_resources
 
 
 def convert(inxml):
 
     dom = ET.parse(inxml)
-    xslt = ET.parse(pkgutil.resource_stream('cobertura-clover-transform',
-                                            'transform.xslt'))
+    xslt = ET.parse(pkg_resources.resource_stream('cobertura_clover_transform',
+                                                  'transform.xslt'))
 
     transform = ET.XSLT(xslt)
     newdom = transform(dom)
@@ -16,7 +16,9 @@ def convert(inxml):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('coverage-xml')
+    parser.add_argument('coverage_xml')
     args = parser.parse_args()
 
     converted = convert(args.coverage_xml)
+
+    print(converted)
