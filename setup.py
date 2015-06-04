fe8f001ce3c5a044e9e5cf0f872474d36d39fe92
Andrew Walker <walker.ab@gmail.com>
2015-06-05 06:59:51 +1000

    added trove classifiers

--- setup.py
+++ setup.py
@@ -12,5 +12,13 @@ setup(
     author_email='cwacek@gmail.com',
     url='http://github.com/cwacek/cobertura-clover-transform',
     license='MIT',
-    keywords='cobertura coverage test clover xml'
+    keywords='cobertura coverage test clover xml',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Testing',
+    ]
 )
