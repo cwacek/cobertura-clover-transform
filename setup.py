e4de6c2f10605fda5537d838d232e4d10e680a28
Chris Wacek <cwacek@gmail.com>
2015-12-08 09:17:18 -0500

    Bump version for trove classifiers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cobertura-clover-transform",
-    version='1.1.4',
+    version='1.1.4.post1',
     packages=find_packages(),
     include_package_data=True,
     description="Tools for transforming Cobertura test "
